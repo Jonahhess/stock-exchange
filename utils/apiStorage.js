7c5817738115e2aa597f3e75fbfc2c70f47fcd99
jonahhess <jonahhessdev@gmail.com>
2025-08-03 22:25:04 +0300

    renamed file

---
+++ utils/apiStorage.js
@@ -0,0 +1,29 @@
+export async function getData(url) {
+  const cached = localStorage.getItem(url);
+  if (cached) return JSON.parse(cached);
+
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("bad path");
+
+  const data = await response.json();
+
+  const stringifiedData = JSON.stringify(data);
+  localStorage.setItem(url, stringifiedData);
+
+  return data;
+}
+
+export async function getFilteredData(url) {
+  const cached = localStorage.getItem(url);
+  if (cached) return JSON.parse(cached);
+
+  const response = await fetch(url);
+  if (!response.ok) throw new Error("bad path");
+
+  const data = await response.json();
+
+  const stringifiedData = JSON.stringify(data);
+  localStorage.setItem(url, stringifiedData);
+
+  return data;
+}
