fbe3c9c0acef05ce01995daf19fcce645c28d686
jonahhess <jonahhessdev@gmail.com>
2025-08-03 22:50:10 +0300

    new fn

--- utils/apiStorage.js
+++ utils/apiStorage.js
@@ -13,14 +13,17 @@ export async function getData(url) {
   return data;
 }
 
-export async function getFilteredData(url) {
+export async function getFilteredData(url, fns) {
   const cached = localStorage.getItem(url);
   if (cached) return JSON.parse(cached);
 
   const response = await fetch(url);
   if (!response.ok) throw new Error("bad path");
 
-  const data = await response.json();
+  let data = await response.json();
+  for (const fn of fns) {
+    data = fn(data);
+  }
 
   const stringifiedData = JSON.stringify(data);
   localStorage.setItem(url, stringifiedData);
